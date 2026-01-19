2c1ebbdac03d30bc1ac4fbb6d3e0792ae4d49bc0
junhee030707-pixel <junhee030707@gmail.com>
2026-01-19 14:36:20 +0900

    실습(1) - character 디자인 + 메인함수 정리

--- Character/Character.cpp
+++ Character/Character.cpp
@@ -1,1 +1,29 @@
 ﻿#include "Character.h"
+
+ACharacter::ACharacter()
+{
+    Name = "UnKnown";
+
+    Hp = 100;
+    Atk = 10;
+
+
+    cout << "ACharacter 생성됨: " << Name << " (HP: " << Hp << ")" << endl;
+}
+
+ACharacter::~ACharacter()
+{
+    cout << "ACharacter 소멸됨" << endl;
+}
+
+void ACharacter::Attack()
+{
+    cout << Name << "가 공격 합니다!" << "공격력 :"; Atk;
+
+}
+
+void ACharacter::TakeDamage(int DamageAmount)
+{
+    Hp -= DamageAmount;
+    cout << Name << "가 " << DamageAmount << "의 피해를 입었습니다." << endl;
+}
--- Character/Character.h
+++ Character/Character.h
@@ -1,5 +1,23 @@
-﻿#pragma once
-class Character
-{
-};
+﻿// 파일명: Character.h
+#pragma once
+#include <iostream>
+#include <string> // 문자열(이름)을 쓰기 위해 필요합니다.
 
+using namespace std;
+
+class ACharacter
+{
+public:
+    ACharacter();
+    ~ACharacter();
+
+protected:
+    string Name;
+    int Hp;
+    int Atk;
+
+public:
+    void Attack();
+    void TakeDamage(int DamageAmount);
+
+};
