50b9ca9a6d80cee4e866f3220e4e6f784fbe481f
junhee030707-pixel <junhee030707@gmail.com>
2026-01-26 11:31:41 +0900

    [실습] 구조체 cpp 먼저 추가

--- Character/Character.cpp
+++ Character/Character.cpp
@@ -1,16 +1,11 @@
 ﻿#include "Character.h"
 
-// [과제 2] 생성자 구현하기
-ACharacter::ACharacter(string NewName, int NewHp, int NewAtk, int NewDef, float NewCritical)
+ACharacter::ACharacter(string NewName, const FUnitStat& NewStat)
 {
-    // 입력받은(New~) 값들을 내 멤버 변수(Name, Hp, Atk)에 저장합니다.
     Name = NewName;
-    Hp = NewHp;
-    Atk = NewAtk;
-    Def = NewDef;
-    Critical = NewCritical;
+    Stat = NewStat;
 
-    cout << "[생성] " << Name << "가 전장에 나타났습니다! (HP: " << Hp << ")" << endl;
+    cout << "[생성] " << Name << "가 전장에 나타났습니다! (HP: " << Stat.Hp << ")" << endl;
 }
 ACharacter::~ACharacter()
 {
@@ -21,54 +16,43 @@ void ACharacter::Attack(ACharacter* Target)
 {
     if (Target == nullptr) return;
 
-    int damage = Atk;
-    bool isCritical = (rand() % 100) < Critical;
+    int damage = Stat.Atk;
+    bool isCritical = (rand() % 100) < Stat.Critical;
 
     if (isCritical)
     {
 
-        damage = static_cast<int>(Atk * 1.5f);  // 크리티컬 배율
+        damage = static_cast<int>(Stat.Atk * 1.5f);  // 크리티컬 배율
         cout << Name << "가 " << Target->GetName() << "을(를) 크리티컬 공격합니다! 공격력: " << damage << endl;
-        Target->TakeDamage(damage);
     }
     else
     {
-        cout << Name << "가 " << Target->GetName() << "을(를) 공격합니다! 공격력: " << Atk << endl;
-        Target->TakeDamage(Atk);
+        cout << Name << "가 " << Target->GetName() << "을(를) 공격합니다! 공격력: " << damage << endl;
     }
 
+    Target->TakeDamage(damage);
 }
 
 void ACharacter::TakeDamage(int DamageAmount)
 {
-    if (Def >= DamageAmount)
+    if (Stat.Def >= DamageAmount)
     {
         DamageAmount = 0;
     }
     else
     {
-        DamageAmount -= Def;
+        DamageAmount -= Stat.Def;
     }
-    Hp -= DamageAmount;
-    cout << Name << "가 " << DamageAmount << "의 피해를 입었습니다." << "Hp :" << Hp << endl;
-}
-
-string ACharacter::GetName()
-{
-    return Name;
-}
-
-int ACharacter::GetHp()
-{
-    return Hp;
+    Stat.Hp -= DamageAmount;
+    cout << Name << "가 " << DamageAmount << "의 피해를 입었습니다." << "Hp :" << Stat.Hp << endl;
 }
 
 bool ACharacter::IsDead()
 {
-    if (Hp <=0) 
+    if (Stat.Hp <= 0)
     {
         return true;
-    } 
+    }
     else
     {
         return false;
