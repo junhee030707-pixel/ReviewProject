cfffbc40e3c846894004623116c366284049fd5e
junhee030707-pixel <junhee030707@gmail.com>
2026-01-26 23:38:28 +0900

    [과제] 피드백

--- Character/Player.h
+++ Character/Player.h
@@ -3,10 +3,26 @@
 #include "Character.h"
 
 
+<<<<<<< Updated upstream
 class APlayer : public ACharacter
 {
 public :
 	APlayer(const string& Name, const FUnitStat& Stat);
+=======
+// Monster Class <- 캐릭터 상속
+// 멤버 - 별도로 추가할 건 없음.
+
+class APlayer : public ACharacter
+{
+public :
+	APlayer(const string& Name, const FUnitStat& Stat)
+		: ACharacter(Name, Stat), Level(1), Exp(0)
+	{
+	}
+	int Level;
+	int Exp;
+
+>>>>>>> Stashed changes
 	void LevelUp();
 	void UseItem();
 	
