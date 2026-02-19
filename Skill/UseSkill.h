0b49a699c21833c3c73754a1415c1ff076a5db89
junhee030707-pixel <junhee030707@gmail.com>
2026-02-19 12:30:20 +0900

    [실습] 생성자 생성 및 Useskill cpp, h 생성자 정의

---
+++ Skill/UseSkill.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+class ACharacter;
+
+class UseSkill
+{
+public:
+	UseSkill(ACharacter* Owner) : Owner(Owner) 
+	{
+	}
+	virtual ~UseSkill(){}
+	virtual void Play(ACharacter* Target) = 0;
+	
+
+protected:
+	ACharacter* Owner;
+};
