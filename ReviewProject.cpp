8499cff070657e91cf9f6f1dee36aecd5ee76309
junhee030707-pixel <junhee030707@gmail.com>
2026-02-19 11:01:57 +0900

    [실습] uniqueptr 사용(스마트 포인터)

--- ReviewProject.cpp
+++ ReviewProject.cpp
@@ -4,72 +4,29 @@
 #include "Character/Character.h"
 #include "Character/Monster.h"
 #include "Character/Player.h"
+#include "Character/ABattleManager.h"
+#include <memory> 
 
-class BattleManager
-{
-    //함수 종류
-
-    void  WaitForPlayerInput()
-    {
-        system("pause");
-        cout << endl;
-    }
-
-    bool BattleTurn(ACharacter* Attacker, ACharacter* Defender)
-    {
-        Attacker->DoAction(Defender);
-        Attacker->ShowStat();
-        Defender->ShowStat();
-        WaitForPlayerInput();
-
-        while (Defender->IsDead())
-        {
-            return true;
-        }
-    }
-
-    void RunBattle() 
-    {
-        BattleTurn();
-    }
-
-    //RunBattle() // 런 배틀 안에선 배틀턴이랑 WaitForPlayerInput이 내부에서 
-    // main()문 내용처럼 호출된다~~~
-    //RunBattle() 함수의 파라미터는 고민해보세요.
-
-    //BattleTurn
-    //WaitForPlayerInput
-};
+using namespace std;
 
 int main()
 {
-    srand(static_cast<unsigned int>(time(nullptr)));
+	auto BattleManager = make_unique<ABattleManager>();
 
-    //객채는 플레이어, 몬스터지만 :: 부모클래스ACharacter의 포인터로 사용한다.
-    ACharacter* Player = new APlayer("나의 용사", FUnitStat(200, 10, 30, 5, 10));
-    ACharacter* Monster = new AMonster("무서운 오크", FUnitStat(100, 10, 20, 3, 10));
+	FUnitStat PlayerStat(200, 50, 30, 5, 10);
+	FUnitStat MonsterStat(100, 30, 20, 3, 10);
 
-    cout << "===  데스매치 시작!  ===" << endl;
-    WaitForPlayerInput();
+	unique_ptr<ACharacter> Player = make_unique<APlayer>("용사", PlayerStat);
+	unique_ptr<ACharacter> Monster = make_unique<AMonster>("몬스터", MonsterStat);
 
-    while (true)
-    {
-        // 1. 플레이어의 턴 (게임 끝났으면 break)
-        if (BattleTurn(Player, Monster) == true)
-        {
-            break;
-        }
+	cout << "===  데스매치 시작!  ===" << endl;
 
-        // 2. 몬스터의 턴 (게임 끝났으면 break)
-        if (BattleTurn(Monster, Player) == true)
-        {
-            break;
-        }
-    }
+	BattleManager->WaitForPlayerInput();
+	BattleManager->RunBattle(Player.get() , Monster.get());
+	BattleManager->WaitForPlayerInput();
 
-    delete Player;
-    delete Monster;
+	cout << "===  데스매치 종료!  ===" << endl;
 
-    WaitForPlayerInput();
-    return 0;
+	return 0;
 }
+
